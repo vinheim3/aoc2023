a86b44dcbcc4f15e0f502123b4a9e47752086a2a
Daniel Jianoran <djianoran@crowdcomms.co.uk>
2023-12-11 05:54:00 +0000

    Change template

--- template.py
+++ template.py
@@ -1,11 +1,24 @@
-from common import get_input
+from collections import *
+from functools import *
+from itertools import *
+from math import *
+
+from common import *
+
+inputs = """
+""".splitlines()
 
 
 def part1():
-    pass
+    data = get_input()
+    # data = get_groups(data)
+    for entry in data:
+        pass
 
 
 def part2():
+    data = get_input()
+    # data = get_groups(data)
     pass
 
 
