f3d4adc677abec70aab17b2ba3f3e12eb35f5fc1
Daniel Jianoran <djianoran@crowdcomms.co.uk>
2023-12-11 05:54:36 +0000

    Change common utils

--- common.py
+++ common.py
@@ -2,3 +2,16 @@ def get_input(day, part=0):
     with open(f"inputs/{day:02}_{part}.txt") as f:
         lines = f.read().splitlines()
     return lines
+
+
+def get_groups(data):
+    curr_group = []
+    groups = []
+    for line in data:
+        if not line:
+            groups.append(curr_group)
+            curr_group = []
+        else:
+            curr_group.append(line)
+    groups.append(curr_group)
+    return groups
