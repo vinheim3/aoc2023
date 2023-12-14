60389252e09b43b408e49312146203326de6feaa
Daniel Jianoran <djianoran@crowdcomms.co.uk>
2023-12-14 12:00:23 +0000

    Do all 4 moves for day 14 part 2

--- day14.py
+++ day14.py
@@ -28,34 +28,29 @@ def move_down(col: tuple):
 
 if __name__ == "__main__":
     grid = Grid(get_input(14))
-    move = 0  # N W S E
     seen = {}
     loopi = 0
 
     while 1:
-        match move:
-            case 0:
-                for i, col in enumerate(grid.get_cols()):
-                    grid.set_col(i, list(move_up(tuple(col))))
-            case 1:
-                for i, row in enumerate(grid.get_rows()):
-                    grid.set_row(i, list(move_up(tuple(row))))
-            case 2:
-                for i, col in enumerate(grid.get_cols()):
-                    grid.set_col(i, list(move_down(tuple(col))))
-            case 3:
-                for i, row in enumerate(grid.get_rows()):
-                    grid.set_row(i, list(move_down(tuple(row))))
+        for i, col in enumerate(grid.get_cols()):
+            grid.set_col(i, list(move_up(tuple(col))))
+        if loopi == 0:
+            print("Part 1:", get_load(grid))
+        for i, row in enumerate(grid.get_rows()):
+            grid.set_row(i, list(move_up(tuple(row))))
+        for i, col in enumerate(grid.get_cols()):
+            grid.set_col(i, list(move_down(tuple(col))))
+        for i, row in enumerate(grid.get_rows()):
+            grid.set_row(i, list(move_down(tuple(row))))
 
         curr = tuple(tuple(row) for row in grid.grid)
         if curr in seen:
             loop_count = loopi - seen[curr]
             first = seen[curr]
-            bill_same_idx = ((4_000_000_000 - 1 - first) % loop_count) + first
-            print("Part 1:", get_load(Grid([k for k, v in seen.items() if v == 0][0])))
+            bill_same_idx = ((1_000_000_000 - 1 - first) % loop_count) + first
+
             print("Part 2:", get_load(Grid([k for k, v in seen.items() if v == bill_same_idx][0])))
             break
         else:
             seen[curr] = loopi
-        move = (move + 1) % 4
         loopi += 1
